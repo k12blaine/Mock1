5cf300ed103c069b87eac4207a50339adab16eb7
daroush <dpeace8@gmail.com>
2018-10-02 15:33:03 -0400

    need to fix error with outputing textfields

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ app.listen(3000,() => {
 
 app.get('/', (req, res) => res.sendFile(__dirname+'/client/index.html'));//connects the backend towards the front end. make sure to plus the folder than contain the index.html
 
-app.get('/contact', (req, res) => res.sendFile(__dirname+'/client/contact.html'));//connects the backend towards the front end. make sure to plus the folder than contain the index.html
+app.get('/contact', (req, res) => {
+  res.sendFile(__dirname+'/client/contact.html');
+  console.log(req.body);
+}
+);//connects the backend towards the front end. make sure to plus the folder than contain the index.html
 
 //sets view engine
 app.use(express.static(__dirname+'/client'));
+
+//app.listen(3000,() => {
+   //console.log(firstname) //you will get your data in this as object.
+
+  // let inputContent = req.body.firstname;
+ //console.log(inputContent);
+//})
